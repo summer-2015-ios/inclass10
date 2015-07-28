a7933f577f1fbacda74a9b0436fab0b3cc31a068
student <student@ccibio104-206.uncc.edu>
2015-07-28 17:09:02 -0400

    Initial Commit

---
+++ InClass10/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InClass10
+//
+//  Created by student on 7/28/15.
+//  Copyright (c) 2015 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InClass10/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InClass10
+//
+//  Created by student on 7/28/15.
+//  Copyright (c) 2015 student. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
